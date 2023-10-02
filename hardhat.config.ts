84d9d3e82f21624df18c6057957369963f7652a1
Gabriel Temtsen <gabrieltemtsen@gmail.com>
2023-10-02 14:24:24 +0100

    deploy config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,17 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+require("dotenv").config();
+
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  networks: {
+    swisstronik: {
+      // If you're using local testnet, replace `url` with local json-rpc address
+      url: "https://json-rpc.testnet.swisstronik.com/",
+      accounts: [`0x` + `${process.env.PRIVATE_KEY}`],
+    },
+  },
 };
 
 export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,26 +1,13 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const perc20 = await ethers.deployContract("PrivateNaira");
+  await perc20.deployed();
 
-  const lockedAmount = ethers.parseEther("0.001");
-
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  console.log(`PrivateNaira was deployed to ${perc20.address}`);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
+//enabling asychronous 
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
